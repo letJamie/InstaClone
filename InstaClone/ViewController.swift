11a2a6ea678e47eb75fc4d4d4f32d7fe6da99ec3
letJamie <stillbut@naver.com>
2020-10-03 00:41:26 +0900

    Create User with Firebase

--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var emailText: UITextField!
     
     @IBOutlet weak var passwordText: UITextField!
@@ -18,12 +19,41 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func signInClicked(_ sender: Any) {
         
         performSegue(withIdentifier: "toFeedVC", sender: nil)
     }
     @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authData, error) in
+                
+                if error != nil {
+                    
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error" )
+                } else {
+                    
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        } else {
+            
+            makeAlert(titleInput: "Error", messageInput: "username/password?")
+        }
+        
+        
+        
+        
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
