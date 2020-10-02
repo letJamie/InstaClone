49817785fd553aad0e880d655c8918983c293e88
letJamie <stillbut@naver.com>
2020-10-02 23:09:02 +0900

    Set up ViewController

---
+++ InstaClone/SettingsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsViewController.swift
+//  InstaClone
+//
+//  Created by Jamie on 2020/10/02.
+//  Copyright © 2020 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logOutClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
